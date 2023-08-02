d032e4f6fd668e8af9b0518b93bdcc3789d88c24
anitha-anii <anithachowdary355@gmail.com>
2023-08-02 15:09:52 +0530

    'changes'

--- src/components/ConfirmationPage.js
+++ src/components/ConfirmationPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 const ConfirmationPage = () => {
+  let navigator=useNavigate();
   const bookingDetails = useSelector((state) => state.flight.bookingDetails);
 
+  function backtohome(){
+    navigator('./components/Landingpage');
+        }
   return (
-    <div>
+
+       <div>
       <h2>Booking Confirmation</h2>
       <p>Your flight has been successfully booked!</p>
-      {/* Display the confirmation details */}
+     
+      <button onClick={backtohome}>Back to Home</button>
     </div>
   );
 };
